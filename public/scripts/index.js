5452a67b5c090e957059c363ee05597cad3675ed
Mazidul Islam <islammazidul1369@gmail.com>
2025-01-14 00:27:39 +0530

    Updated projects and work experience section

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -11,7 +11,7 @@ $(window).on("scroll", function () {
     }
 });
 
-/*-------------------------Page Scrollibg - ScrollIt------------------------*/
+/*-------------------------Page Scrolling - ScrollIt------------------------*/
 $.scrollIt({
     topOffset: -50,
 });
